31ce6a3feb8115cdf7765a8c5c05febce6aa9532
Vaibhav Patel <vaibhav.p2305@gmail.com>
2022-11-25 20:37:47 -0500

    Implemented chatting

--- app/src/main/java/com/example/ridex/InboxPageFragment.java
+++ app/src/main/java/com/example/ridex/InboxPageFragment.java
@@ -3,62 +3,81 @@ package com.example.ridex;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link InboxPageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.ridex.models.ChatRoom;
+import com.example.ridex.models.Messages;
+import com.example.ridex.models.Posts;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+
 public class InboxPageFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String ACTIVITY_NAME = "InboxPageFragment";
+    // UI variables
+    RecyclerView messageInboxRecyclerView;
+    // MongoDB Realm
+    Realm realm;
+    App app;
+    RealmResults<ChatRoom> myInbox;
 
     public InboxPageFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InboxPageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InboxPageFragment newInstance(String param1, String param2) {
-        InboxPageFragment fragment = new InboxPageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        app = new App(new AppConfiguration.Builder(MongoDb.appId).build());
+        realm = MainActivity.getRealm(getActivity());
+
+        myInbox = realm.where(ChatRoom.class)
+                .equalTo("userOneUID", app.currentUser().getId())
+                .or()
+                .equalTo("userTwoUID", app.currentUser().getId())
+                .findAll();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_inbox_page, container, false);
+        messageInboxRecyclerView = view.findViewById(R.id.messagesRecyclerView);
+
+        if (myInbox.size() == 0){
+            emptyPostingViews();
+        } else {
+//            Log.i(ACTIVITY_NAME, "Inbox: " + String.valueOf(myInbox));
+            displayPostingViews(myInbox);
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inbox_page, container, false);
+        return view;
     }
+
+    public void displayPostingViews(RealmResults<ChatRoom> myInbox){
+        InboxRealmAdapter inboxRealmAdapter =
+                new InboxRealmAdapter(getContext(), myInbox, realm, app.currentUser().getId());
+        messageInboxRecyclerView.setAdapter(inboxRealmAdapter);
+        messageInboxRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        inboxRealmAdapter.notifyDataSetChanged();
+    }
+
+    public void emptyPostingViews(){
+        EmptyPostingAdapter emptyPostingAdapter = new EmptyPostingAdapter(getContext(),
+                "No messages to show yet!");
+        messageInboxRecyclerView.setAdapter(emptyPostingAdapter);
+        messageInboxRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+
 }
---
+++ app/src/main/java/com/example/ridex/MessagesRealmAdapter.java
@@ -0,0 +1,107 @@
+package com.example.ridex;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ridex.models.ChatRoom;
+import com.example.ridex.models.Messages;
+import com.example.ridex.models.Users;
+
+import org.bson.types.ObjectId;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+public class MessagesRealmAdapter
+        extends RealmRecyclerViewAdapter<Messages,
+        RecyclerView.ViewHolder> {
+    public static final String ACTIVITY_NAME = "MessagesRealmAdapter";
+
+    Context context;
+    RealmResults<Messages> inboxList;
+    String currentUserUID;
+
+    public MessagesRealmAdapter(Context context_taken, RealmResults<Messages> inbox, String currentUser){
+        super(inbox, true, true);
+        context = context_taken;
+        inboxList = inbox;
+        currentUserUID = currentUser;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (inboxList.get(position).getSenderUID().equals(currentUserUID)){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        switch (viewType) {
+            case 0:
+                View view = inflater.inflate(R.layout.sent_message_container, parent, false);
+                return new SenderViewHolder(view);
+            case 1:
+                View view2 = inflater.inflate(R.layout.received_message_container, parent, false);
+                return new ReceiverViewHolder(view2);
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        switch (holder.getItemViewType()){
+            case 0:
+                // Sending
+                SenderViewHolder senderViewHolder = (SenderViewHolder) holder;
+                senderViewHolder.sentMessage.setText(inboxList.get(position).getMessageSent());
+                senderViewHolder.sentMessageTimeDate.setText(inboxList.get(position).getTime());
+                break;
+            case 1:
+                // Receiving
+                ReceiverViewHolder receiverViewHolder = (ReceiverViewHolder) holder;
+                receiverViewHolder.receivedMessage.setText(inboxList.get(position).getMessageSent());
+                receiverViewHolder.receivedMessageTimeDate.setText(inboxList.get(position).getTime());
+                break;
+        }
+    }
+
+    public class SenderViewHolder extends RecyclerView.ViewHolder {
+        TextView sentMessage, sentMessageTimeDate;
+
+        public SenderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // UI Variables
+            sentMessage = itemView.findViewById(R.id.sentMessage);
+            sentMessageTimeDate = itemView.findViewById(R.id.sentMessageTimeDate);
+        }
+    }
+
+    public class ReceiverViewHolder extends RecyclerView.ViewHolder {
+        TextView receivedMessage, receivedMessageTimeDate;
+        ImageView profilePhoto;
+
+        public ReceiverViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // UI Variables
+            receivedMessage = itemView.findViewById(R.id.receivedMessage);
+            receivedMessageTimeDate = itemView.findViewById(R.id.receivedMessageTimeDate);
+            profilePhoto = itemView.findViewById(R.id.profilePhoto);
+        }
+    }
+}
